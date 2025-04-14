053c5d140ae635fdd0e29cd068588029d01362ac
12 tec <compras@12tec.com.br>
2025-04-14 14:31:05 -0300

    Mensagem do commit

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+module.exports = {
+    siteUrl: 'https://diagnosticoesg.com.br',  // Substitua pela URL do seu site
+    generateRobotsTxt: true,  // Para gerar automaticamente um arquivo robots.txt
+    sitemapSize: 7000,  // Tamanho máximo de URLs por sitemap
+    changefreq: 'daily',  // Frequência de atualização das páginas
+    priority: 0.7,  // Prioridade das páginas
+  };
